43be8b41400a3c7cffc5d06f8758b118e3477d4e
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-06-12 07:14:33 +0100

    feat: create hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ContactForm } from 'components/forms/contactForm/ContactForm';
 import { Section } from 'components/section/Section';
-
+import { HeroSection } from 'presenters/HeroSection';
 
 function App() {
   return (
     <div className="main__wrapper">
+      <HeroSection />
       <Section theme="grey">
         <div className='grid grid-cols-1 lg:grid-cols-8 mx-auto'>
           <div className='col-span-8 lg:col-start-2 lg:col-span-6'>
---
+++ src/presenters/HeroSection.tsx
@@ -0,0 +1,22 @@
+import { Section } from "components/section/Section";
+import DroneImage from "assets/images/Drone.png";
+
+export const HeroSection = () => {
+  return (
+    <Section navType="one" theme="grey">
+      <div className="flex flex-col justify-center items-center">
+        <img src={DroneImage} alt="drone" className="w-full md:w-[38%] object-cover" />
+        <h1 className="text-7xl md:text-7xl lg:text-xxl font-black text-white tracking-wide mt-6 leading-tight md:leading-none">
+          F-210
+        </h1>
+        <p className="text-[#dbd9d3] text-xxs md:text-sm uppercase tracking-wide">
+          "The fastest Drone on the planet"
+        </p>
+        <p className="text-[#dbd9d3] text-xxs md:text-sm tracking-wide mt-1 mb-20">
+          "The- The Wirecutter
+        </p>
+      </div>
+    </Section>
+  );
+};
+
