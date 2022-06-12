9c4261cfb535e7c39f5c68b2ef1ca17822251212
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-06-12 01:46:16 +0100

    chore: input reuseable components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import { Button } from "components/button/Button";
-
+import { Input } from 'components/forms/shared/input/Input';
+import { Textarea } from 'components/forms/shared/textarea/Textarea';
 
 function App() {
   return (
     <div>
+      <Input
+        labelText="Email"
+        name="email"
+        id="email"
+        placeholder="Enter your email"
+        type="email"
+        onChange={() => {}}
+        value="email"
+      />
+      <Textarea 
+        labelText="Message"
+        name="message"
+        id="message"
+        placeholder="What are your plans?"
+        onChange={() => {}}
+        value="message"
+      />
      <Button type="submit" variant="primary" size='medium'>
         Send Message
       </Button>
