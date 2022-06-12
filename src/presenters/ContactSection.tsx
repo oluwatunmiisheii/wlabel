d3d24e106eb0b04666fa7e17cf7de94be2e3b368
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-06-12 13:36:52 +0100

    feat: add product details component

--- src/presenters/ContactSection.tsx
+++ src/presenters/ContactSection.tsx
@@ -4,7 +4,7 @@ import { Section } from "components/section/Section";
 export const ContactSection = () => {
   return (
     <Section theme="grey">
-      <div className='grid grid-cols-1 lg:grid-cols-8 mx-auto'>
+      <div className='w-full grid grid-cols-1 lg:grid-cols-8 mx-auto'>
         <div className='col-span-8 lg:col-start-2 lg:col-span-6'>
           <ContactForm />
         </div>
--- src/presenters/HeroSection.tsx
+++ src/presenters/HeroSection.tsx
@@ -4,7 +4,7 @@ import DroneImage from "assets/images/Drone.png";
 export const HeroSection = () => {
   return (
     <Section navType="one" theme="grey">
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center pt-16">
         <img src={DroneImage} alt="drone" className="w-full md:w-[38%] object-cover" />
         <h1 className="text-7xl md:text-7xl lg:text-xxl font-black text-white tracking-wide mt-6 leading-tight md:leading-none">
           F-210
