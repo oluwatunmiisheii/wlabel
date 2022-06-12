3449c1b9523b2630edc9b0ff01397e076772325b
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-06-12 07:19:17 +0100

    refactor: change contact section to make use of presenter structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { ContactForm } from 'components/forms/contactForm/ContactForm';
-import { Section } from 'components/section/Section';
+import { ContactSection } from 'presenters/ContactSection';
 import { HeroSection } from 'presenters/HeroSection';
 
 function App() {
   return (
     <div className="main__wrapper">
       <HeroSection />
-      <Section theme="grey">
-        <div className='grid grid-cols-1 lg:grid-cols-8 mx-auto'>
-          <div className='col-span-8 lg:col-start-2 lg:col-span-6'>
-            <ContactForm />
-          </div>
-        </div>
-      </Section>
+      <ContactSection />
     </div>
   );
 }
