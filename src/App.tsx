d850baad17294a4f0538d733da6a976cb50a4ed5
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-06-13 02:40:36 +0100

    chore: improve responsiveness of product details page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import { ContactSection } from 'presenters/ContactSection';
 import { HeroSection } from 'presenters/HeroSection';
 import { ProductDetailsSection } from 'presenters/ProductDetailsSection';
 import { productDetails } from "db/ProductDetails"
-import { useId } from 'react';
 
 function App() {
-  const id = useId();
-  
   return (
     <div className="main__wrapper">
       <HeroSection />
       {productDetails.map((details) => (
-        <ProductDetailsSection {...details} key={id} />
+        <ProductDetailsSection {...details} key={details.title} />
       ))}
       <ContactSection />
     </div>
